0ff06368c23bfd03e19e167e4358fd5a84a470c7
ThanhDatTu3tle <71086168+ThanhDatTu3tle@users.noreply.github.com>
2022-10-19 17:13:07 +0700

    update config database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import config from '../ormconfig';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({ ...config, autoLoadEntities: true }), 
+    UsersModule, ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
